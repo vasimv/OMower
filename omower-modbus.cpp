4233c64ae469da67312e333356486b4060ce3808
vasimv <vasimv@gmail.com>
2018-04-05 14:52:13 -0400

    Added putModbus16 and putModbus32 functions

--- omower-modbus.cpp
+++ omower-modbus.cpp
@@ -5,7 +5,7 @@
 #include <string.h>
 
 // Calculate CRC for modbus frame                                                                             
-uint16_t modbusCrc(char *buf, int len) {                                                                   
+uint16_t modbusCrc(uint8_t *buf, int len) {                                                                   
   uint32_t tmp, tmp2;                                                                                         
   uint8_t Flag;                                                                                               
   uint16_t i, j;                                                                                            
@@ -24,16 +24,30 @@ uint16_t modbusCrc(char *buf, int len) {
   tmp = (tmp << 8) | tmp2;                                                                                  
   tmp &= 0xFFFF;                                                                                            
   return (uint16_t) tmp;                                                                                    
-} // uint16_t modbusCrc(byte *buf, int len)    
+} // uint16_t modbusCrc(uint8_t *buf, int len)    
 
 // Retrieve 16-bit value from modbus frame
-uint16_t getModbus16(char *addr) {
+uint16_t getModbus16(uint8_t *addr) {
   return (uint16_t) addr[1] | ((uint16_t) addr[0] << 8);
-} // uint16_t getModbus16(char *addr)
+} // uint16_t getModbus16(uint8_t *addr)
 
 // Retrieve 32-bit value from modbus frame (two 16bit registers joined)
-uint32_t getModbus32(char *addr) {
+uint32_t getModbus32(uint8_t *addr) {
   return (uint32_t) addr[3] | ((uint32_t) addr[2] << 8)
           | ((uint32_t) addr[1] << 16) | ((uint32_t) addr[0] << 24);
-} // uint32_t getModbus32(char *addr)
+} // uint32_t getModbus32(uint8_t *addr)
+
+// Put 16-bit value to modbus frame buffer (must increase pointer +2 after)
+void putModbus16(uint8_t *buf, uint16_t val) {
+  buf[0] = (val >> 8) & 0xff;
+  buf[1] = val & 0xff;
+} // void putModbus16(uint8_t *buf, uint16_t val)
+
+// Put 32-bit value to modbus frame buffer (must increase pointer +4 after)
+void putModbus32(uint8_t *buf, uint32_t val) {
+  buf[0] = (val >> 24) & 0xff;
+  buf[1] = (val >> 16) & 0xff;
+  buf[2] = (val >> 8) & 0xff;
+  buf[3] = val & 0xff;
+} // void putModbus32(uint8_t *buf, uint32_t val)
 
--- omower-modbus.h
+++ omower-modbus.h
@@ -7,12 +7,18 @@
 #include <stdint.h>
 
 // Calculate CRC for modbus frame                                                                             
-uint16_t modbusCrc(char *buf, int len);
+uint16_t modbusCrc(uint8_t *buf, int len);
 
 // Retrieve 16-bit value from modbus frame
-uint16_t getModbus16(char *addr);
+uint16_t getModbus16(uint8_t *addr);
 
 // Retrieve 32-bit value from modbus frame (two 16bit registers joined)
-uint32_t getModbus32(char *addr);
+uint32_t getModbus32(uint8_t *addr);
+
+// Put 16-bit value to modbus frame buffer (must increase pointer +2 after)
+void putModbus16(uint8_t *buf, uint16_t val);
+
+// Put 32-bit value to modbus frame buffer (must increase pointer +4 after)
+void putModbus32(uint8_t *buf, uint32_t val);
 
 #endif
